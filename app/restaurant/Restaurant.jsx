c66e3d754d6099a8cfff03039254a6477902e08d
mohamedkhameSss <khamessm24@gmail.com.com>
2024-03-31 16:02:37 +0200

    restaurantPage1

--- app/restaurant/Restaurant.jsx
+++ app/restaurant/Restaurant.jsx
@@ -1,16 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { useRoute } from '@react-navigation/native'
 import NetworkImage from '../components/NetworkImage';
 import RestaurantPage from '../navigation/RestaurantPage';
-import { SIZES } from '../constants/theme';
-
+import { COLORS, SIZES } from '../constants/theme';
+import { Ionicons,MaterialCommunityIcons ,AntDesign} from '@expo/vector-icons';
 const Restaurant = ({navigation}) => {
     const route= useRoute();
     const item= route.params;
     console.log(item);
   return (
     <View>
+      <View>
+        <TouchableOpacity onPress={()=>navigation.goBack()} 
+        style={styles.backtn} >
+          <Ionicons name='chevron-back-circle' size={30} color={COLORS.primary}/>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={()=>{}} 
+        style={styles.sharetn}
+        ><MaterialCommunityIcons name='share-circle' size={30} color={COLORS.primary}/>
+        </TouchableOpacity>
+        <View style={styles.rating}></View>
+        </View>
       <NetworkImage source={item.imageUrl} height={SIZES.height/3.4} width={SIZES.width} radius={15} />
       <View style={{height:200}}></View>
       <View style={{height:400}}>
@@ -22,4 +33,35 @@ const Restaurant = ({navigation}) => {
 
 export default Restaurant
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  backtn:{
+    marginLeft:12,
+    alignItems:'center',
+    zIndex:999,
+    top:SIZES.xxLarge,
+    position:'absolute'
+    
+  },
+  sharetn:{
+    marginRight:12,
+    alignItems:'center',
+    zIndex:999,
+    right:0,
+    top:SIZES.xxLarge+3,
+    position:'absolute'
+    
+  },    title:{
+    fontSize:22,
+    fontFamily:'medium',
+    color:COLORS.black,
+  },rating:{
+    height:50,
+    justifyContent:'center',
+    width:'100%',
+    position:"absolute",
+    backgroundColor:'#00ff53c',
+    zIndex:999,
+    bottom:0,
+    borderRadius:15,
+  }
+})
